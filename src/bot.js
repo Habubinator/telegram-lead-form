6b36cb0fd1338442eba20f09a1aafc60f37cb501
Habubinator <61784574+Habubinator@users.noreply.github.com>
2024-05-09 20:26:05 +0300

    test

--- src/bot.js
+++ src/bot.js
@@ -8,21 +8,37 @@ const logger = require("./logger.js");
 const questions = require("./questions.json");
 const users = new Map();
 
+function jsonToBoldString(jsonObj) {
+    let result = "";
+    let i = 0;
+    for (const key in jsonObj) {
+        if (jsonObj.hasOwnProperty(key)) {
+            result += `<b>${key}</b>: ${jsonObj[key]}\n`;
+            if (i == 3) {
+                result += `\n\n`;
+            }
+        }
+        i++;
+    }
+    return result;
+}
+
 bot.setMyCommands([{ command: "/start", description: "Запустити бота" }]);
 
 // Роблю все всередині юзкейсу бо 2 команди всього і немає навіщо писати перевірки на команди в bot.on("message")
 bot.on("message", (msg) => {
     try {
         const chatId = msg.chat.id;
-        // Щоб ніхто не зламав скрипт, додаючи бота до чату
-        if (msg.from.id != chatId) {
-            return bot.sendMessage(
-                chatId,
-                "На жаль, бот працює тільки в приватних чатах"
-            );
-        }
+        console.log(msg);
         switch (msg.text) {
             case "/start":
+                // Щоб ніхто не зламав скрипт, додаючи бота до чату
+                if (msg.from.id != chatId) {
+                    return bot.sendMessage(
+                        chatId,
+                        "На жаль, бот працює тільки в приватних чатах"
+                    );
+                }
                 users.set(msg.from.id, {
                     user: msg.from,
                     progress: 0,
@@ -76,9 +92,31 @@ bot.on("message", (msg) => {
                         logger.logItems(user);
                         users.delete(msg.from.id);
                         gSh.addUserResults(user);
+
+                        let stringUser =
+                            `Форму заповнив новий користувач\n` +
+                            jsonToBoldString({
+                                "chatID користувача": user.user.id,
+                                "ID користувача": user.user.username,
+                                "1 питання": user.answers[1],
+                                "2 питання": user.answers[2],
+                                "3 питання": user.answers[3],
+                                "4 питання": user.answers[4],
+                                "5 питання": user.answers[5],
+                                "Номер телефону": user.answers[6],
+                                "Ім'я": user.answers[7],
+                            });
+                        bot.sendMessage(
+                            process.env.CHAT_TO_RECEIVE_MSG,
+                            stringUser,
+                            {
+                                parse_mode: `HTML`,
+                            }
+                        );
+
                         return bot.sendMessage(
                             chatId,
-                            "Дякую вам за відповіді!",
+                            "Ваша заявка відправлена!",
                             {
                                 reply_markup: {
                                     inline_keyboard: [
